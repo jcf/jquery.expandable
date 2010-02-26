c2fe933f8453e690ad73ceb46e5f7228572bdff5
James Conroy-Finn <james@logi.cl>
2010-02-26 17:06:53 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,42 @@
+## jquery.expandable
+
+Created by James Conroy-Finn.
+
+This plugin is ultimately simple, it takes no arguments and assumes you simply want to have a "view port" hide the contents of a big child until a user clicks on the "view port".
+
+### Usage
+
+Wrap your large, element in a DIV...
+
+    <div class="view_port">
+      <div>
+        <h1>Some content</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur magna aliqua. Ut enim ad minim 
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+          velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint 
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+          mollit anim id est mega peng
+        </p>
+      </div>
+    </div>
+
+Providing a height for the view port in your CSS and add some optional styles to use when your "view port" is expanded...
+
+    .view_port { height: 18px; background-color: #eee; }
+    .view_port.expanded { background-color: #fff };
+
+Or in your SASS if you're anything like me!
+
+    .view_port
+      height: 18px
+      background-color: #eee
+
+      &.expanded
+        background-color: #fff
+
+Then in your JS somewhere call expandable on a jQuery selection...
+
+    $('.view_port').expandable();
+
---
+++ jquery.expandable.js
@@ -0,0 +1,45 @@
+/*
+ * jQuery Expandable
+ * (c) James Conroy-Finn 2010, http://jamesconroyfinn.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+jQuery(function($) {
+  $.fn.expandable = function() {
+    return this.each(function() {
+      (function($view_port) {
+        var view_port_height = $view_port.outerHeight();
+        var element_height = $view_port.children().first().outerHeight();
+
+        if (view_port_height <= element_height) { 
+          $view_port.addClass('expandable');
+          $view_port.click(function() {
+            height = $(this).hasClass('expanded') ? view_port_height : element_height;
+            $(this).toggleClass('expanded').animate({'height': height});
+          });
+        };
+      })($(this));
+    });
+  }
+});
+
+
